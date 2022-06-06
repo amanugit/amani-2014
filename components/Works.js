015a8cbab9e9c626dd803e3d391610165f1ffe07
Amanuel <amannew49@gmail.com>
2022-06-06 23:11:14 +0300

    first commit

--- components/Works.js
+++ components/Works.js
@@ -1,6 +1,7 @@
 import { Button, Container, Grid, Typography } from "@mui/material";
 import React from "react";
 import wmn from "../public/imgs/wmn.jpg";
+import ss from "../public/imgs/ss.jpg";
 import md from "../public/imgs/md.jpg";
 import zz from "../public/imgs/zz.jpg";
 import na from "../public/imgs/na.jpg";
@@ -229,7 +230,7 @@ function Works() {
               }}
             >
               <Image
-                src={wmn}
+                src={ss}
                 sizes="100vw"
                 layout="responsive"
                 objectFit="cover"
